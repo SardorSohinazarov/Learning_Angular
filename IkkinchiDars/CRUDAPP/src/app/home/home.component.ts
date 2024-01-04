db55496d4bdede5d15543b3521dbba00c07be273
SardorSohinazarov <sardorstudent0618gmail.com>
2024-01-04 10:02:42 +0500

    Houses

---
+++ IkkinchiDars/CRUDAPP/src/app/home/home.component.ts
@@ -0,0 +1,91 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    CommonModule,
+    HousingLocationComponent
+  ],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city">
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housing-location *ngFor="let housingLocation of housingLocationList" [housingLocation]="housingLocation"></app-housing-location>
+    </section>
+  `,
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+
+  housingLocationList: HousingLocation[] = [
+    {
+      id: 9999,
+      name: 'Test Home',
+      city: 'Test city',
+      state: 'ST',
+      photo: `${this.baseUrl}/example-house.jpg`,
+      availableUnits: 99,
+      wifi: true,
+      laundry: false,
+    },
+    {
+      id: 9999,
+      name: 'Test Home',
+      city: 'Test city',
+      state: 'ST',
+      photo: `${this.baseUrl}/example-house.jpg`,
+      availableUnits: 99,
+      wifi: true,
+      laundry: false,
+    },
+    {
+      id: 9999,
+      name: 'Test Home',
+      city: 'Test city',
+      state: 'ST',
+      photo: `${this.baseUrl}/example-house.jpg`,
+      availableUnits: 99,
+      wifi: true,
+      laundry: false,
+    },
+    {
+      id: 9999,
+      name: 'Test Home',
+      city: 'Test city',
+      state: 'ST',
+      photo: `${this.baseUrl}/example-house.jpg`,
+      availableUnits: 99,
+      wifi: true,
+      laundry: false,
+    },
+    {
+      id: 9999,
+      name: 'Test Home',
+      city: 'Test city',
+      state: 'ST',
+      photo: `${this.baseUrl}/example-house.jpg`,
+      availableUnits: 99,
+      wifi: true,
+      laundry: false,
+    },
+    {
+      id: 9999,
+      name: 'Test Home',
+      city: 'Test city',
+      state: 'ST',
+      photo: `${this.baseUrl}/example-house.jpg`,
+      availableUnits: 99,
+      wifi: true,
+      laundry: false,
+    },
+  ];
+}
